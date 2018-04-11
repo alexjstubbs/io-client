3d9234a4b53e5d1d88dac78ab063f6c8a5acc868
Alex Stubbs <alexjstubbs@users.noreply.github.com>
2018-04-11 18:33:19 -0400

    Navigation Shapes Backgrounds

--- src/components/NavBlockAddGames.jsx
+++ src/components/NavBlockAddGames.jsx
@@ -4,7 +4,7 @@ const Components = {
   NavBlockExplorer: function NavBlockExplorer(props) {
     return (
      
-          <div className="navigation-button shapes h-30-vh twelve columns">
+          <div className="navigation-button h-30-vh twelve columns">
               <h4 className="nopadding-nomargin">Add New Games</h4>
               <span className="mute nopadding-nomargin">Add games to your library</span>
           </div>
--- src/components/NavBlockExplorer.jsx
+++ src/components/NavBlockExplorer.jsx
@@ -4,7 +4,7 @@ const Components = {
   NavBlockExplorer: function NavBlockExplorer(props) {
     return (
      
-          <div className="selectedNav navigation-button h-40-vh twelve columns">
+          <div className="selectedNav navigation-button shapes h-40-vh twelve columns">
               <h4 className="nopadding-nomargin">Game Explorer</h4>
               <span className="mute nopadding-nomargin">Browse your game collection</span>
           </div>
